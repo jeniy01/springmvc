d2771e65734fbdc6c0fac90c65688050e1b710d1
jeniy01 <dallown@naver.com>
2023-06-19 09:33:28 +0900

    springmvc

---
+++ Validator/src/kr/co/tjoeun/validator/TestBean1Validator.java
@@ -0,0 +1,24 @@
+package kr.co.tjoeun.validator;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import kr.co.tjoeun.beans.TestBean1;
+
+public class TestBean1Validator implements Validator {
+	// 유효성 검사할 data를 가지고 있는 객체가 유효성 검사가 가능한지 확인함
+	@Override
+	public boolean supports(Class<?> clazz) {
+		return TestBean1.class.isAssignableFrom(clazz);
+	}
+	
+	//유효성 검사하는 메서드
+	@Override
+	public void validate(Object target, Errors errors) {
+		//rejectIfEmpty(error객체, "property이름", "코드이름")
+		ValidationUtils.rejectIfEmpty(errors, "data2", "errors2");
+		//ValidationUtils.rejectIfEmptyOrWhitespace(error 객체, "property이름", "코드이름");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "data3", "errors3");
+	}
+}
